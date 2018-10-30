9afc7551ecfd9f576d152a5860880648e10ae719
Pablo Pino <pabpinjim@alum.us.es>
2018-10-30 19:06:39 +0100

    Se ha puesto Ticketable como clase abstracta y se han añadido anotaciones

--- HandyWorker L02/Sample-Project-1.18.1/src/main/java/domain/Ticketable.java
+++ HandyWorker L02/Sample-Project-1.18.1/src/main/java/domain/Ticketable.java
@@ -1,24 +1,23 @@
 package domain;
 
 import javax.persistence.Column;
-import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-
-
-public class Ticketable  extends DomainEntity{
+public abstract class Ticketable  extends DomainEntity {
 
 	//----------------Atributos---------------
-	private String	ticker;
-
 	
+	private String	ticker;
 	
 	//--------getters y setters------------
+	
 	@NotBlank
 	@Column(unique=true)
-	@Pattern(regexp="^\\d{6}-([A-Z]|\\d){6}$")x//comprobar patrón
+	@Pattern(regexp="^\\d{6}-([A-Z]|\\d){6}$") //comprobar patrón
+	@NotNull
 	public String getTicker() {
 		return ticker;
 	}
