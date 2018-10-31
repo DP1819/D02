b9cc61479d912cbac039785fbb31bde734941456
Pablo Pino <pabpinjim@alum.us.es>
2018-10-31 13:06:49 +0100

    Se han añadido las anotaciones necesarias a MiscellaneousRecord

--- HandyWorker L02/Sample-Project-1.18.1/src/main/java/domain/MiscellaneousRecord.java
+++ HandyWorker L02/Sample-Project-1.18.1/src/main/java/domain/MiscellaneousRecord.java
@@ -1,6 +1,9 @@
 
 package domain;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
@@ -12,10 +15,16 @@ public class MiscellaneousRecord extends DomainEntity {
 	private String	attachment;
 	private String	comments;
 
+	
+	//----------Relationships---------
+	
+	private Curriculum curriculum;
+	
 
 	//-----------Getters y Setters------
 
 	@NotBlank
+	@NotNull
 	public String getTitle() {
 		return this.title;
 	}
@@ -41,4 +50,14 @@ public class MiscellaneousRecord extends DomainEntity {
 		this.comments = comments;
 	}
 
+	@Valid
+	@NotNull
+	public Curriculum getCurriculum() {
+		return curriculum;
+	}
+
+	public void setCurriculum(Curriculum curriculum) {
+		this.curriculum = curriculum;
+	}
+	
 }
